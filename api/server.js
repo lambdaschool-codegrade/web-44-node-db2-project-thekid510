d9beb8bd03ae84265dc82c349ea40c640b0ea3d1
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-08-10 23:49:32 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ const express = require("express")
 const server = express()
 
 server.use(express.json())
+const carsRouter = require('./cars/cars-router')
 
+server.use('/api/cars',carsRouter)
 server.use('*', (req, res, next)=> {
-    next({ status: 404, message: 'not found'})
+    next({ status: 404, message: 'not found!'})
 })
 
 server.use((err, req, res, next)=> {
