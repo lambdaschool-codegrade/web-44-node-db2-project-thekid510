4bab29d0e8f4e8b33670423cb275a0c2481dc1b6
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-08-10 23:57:52 -0700

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,17 @@
-const checkCarId = (req, res, next) => {
+const Car = require('./cars-model')
+const checkCarId =  async (req, res, next) => {
+  try{
+    const car = await Car.getById(req.params.id)
+    if (!car){
+
+    } else{
+      next()
+    }
+  } catch(err){
+    next(err)
+  }
   // DO YOUR MAGIC
+ 
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +24,10 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid
 }
